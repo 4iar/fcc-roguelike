20a82a380c70fe20401d093ec9b5e15f948e22d2
4iar <4iar@users.noreply.github.com>
2016-07-19 17:45:34 +0100

    Fix bounds checking

--- src/utils/outOfBounds.js
+++ src/utils/outOfBounds.js
@@ -1,3 +1,3 @@
 export default function outOfBounds (board, coordinates) {
-  return board[coordinates[0]] === undefined || board[coordinates[0][coordinates[1]]];
+  return coordinates[0] < 0 || coordinates[1] < 0 || coordinates[0] >= board.length || coordinates[1] >= board.length
 }
