d17ce2ae19a04c07711f52ae9f1f2acaa3d44b29
4iar <4iar@users.noreply.github.com>
2016-07-19 16:32:23 +0100

    Fix formatting

--- src/utils/outOfBounds.js
+++ src/utils/outOfBounds.js
@@ -1,3 +1,3 @@
 export default function outOfBounds (board, coordinates) {
-  return board[coordinates[0]] === undefined || board[coordinates[0][coordinates[1]]]
-};
+  return board[coordinates[0]] === undefined || board[coordinates[0][coordinates[1]]];
+}
