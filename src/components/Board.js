c8fd1bcc2c71f878737e43f7ebbdecf4f4980b0d
4iar <4iar@users.noreply.github.com>
2016-07-20 02:32:25 +0100

    Connect redux to the board view

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,33 +1,48 @@
 import React from 'react';
 import _ from 'lodash';
 import { Col } from 'react-bootstrap';
+import { connect } from 'react-redux';
 
 import '../styles/board.scss';
 
+const getState = (state) => {
+  return {
+    board: state.game.board,
+    playerCoordinates: state.game.player.coordinates
+  };
+};
 
+@connect(getState, null, null, {withRef: true})
 export default class Board extends React.Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      board: [[1, 2, 3, 4, 5], [1, 2, 3, 4, 5], [1, 2, 3, 4, 5], [1, 2, 3, 4, 5], [1, 2, 3, 4, 5]],
+      board: this.props.board,
       viewportSize: [5, 5],
-      viewport: [[1, 2, 3, 4, 5], [1, 2, 3, 4, 5], [1, 2, 3, 4, 5], [1, 2, 3, 4, 5], [1, 2, 3, 4, 5]],
+      playerCoordinates: this.props.playerCoordinates,
     };
   }
 
+  componentWillReceiveProps(newProps) {
+    this.setState({
+      board: newProps.board,
+      playerCoordinates: newProps.playerCoordinates
+    });
+  }
 
   render() {
-    const rowLength = this.state.viewport.length;
-    const colLength = this.state.viewport[0].length;
+    const rowLength = this.state.board.length
+    const colLength = this.state.board[0].length;
 
     let rowElements = [];
     for (let row = 0; row < rowLength; row++) {
       rowElements.push([]);
       for (let col = 0; col < colLength; col++) {
-        rowElements[row].push(<div className="square">{row}: {col}</div>)
+        rowElements[row].push(<div className="square">{this.state.board[row][col].type}</div>)
       }
     }
+
     return (
       <Col xs={11} md={11}>
         <div className="board">
