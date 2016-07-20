7883052377c8c48513e073d464651bd35383865d
4iar <4iar@users.noreply.github.com>
2016-07-20 02:19:51 +0100

    Fix incorrect outOfBounds->true happening on row movements

--- src/utils/outOfBounds.js
+++ src/utils/outOfBounds.js
@@ -1,3 +1,3 @@
 export default function outOfBounds (board, coordinates) {
-  return coordinates[0] < 0 || coordinates[1] < 0 || coordinates[0] >= board.length || coordinates[1] >= board.length
+  return coordinates[0] < 0 || coordinates[1] < 0 || coordinates[0] >= board.length || coordinates[1] >= board[0].length
 }
