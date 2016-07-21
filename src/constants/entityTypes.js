8322d2c075b48ba0ef8b4b22874da71933525ff5
4iar <4iar@users.noreply.github.com>
2016-07-21 17:30:40 +0100

    Add ladder entities and constants

--- src/constants/entityTypes.js
+++ src/constants/entityTypes.js
@@ -2,4 +2,4 @@ export const enemyEntity = 'enemy';
 export const obstacleEntity = 'obstacle';
 export const floorEntity = 'floor';
 export const playerEntity = 'player';
-
+export const ladderEntity = 'ladder';
