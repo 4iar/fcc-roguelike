43334621765004fdc7f2ac9d5bb5377e08103ec0
4iar <4iar@users.noreply.github.com>
2016-07-23 02:25:26 +0100

    Clean up enemy entites file

--- src/entities/enemies.js
+++ src/entities/enemies.js
@@ -1,19 +1,19 @@
 import { enemyEntity } from '../constants/entityTypes';
 
-const enemy = {
+const Enemy = {
   name: '',
   health: 1,
   damage: 1,
   xpOnDeath: 1,
-  className: '',
+  className: 'enemy',
   character: '',
-  type: enemyEntity
+  type: enemyEntity,
 };
 
-export const DollarEnemy = {...enemy,
+export const DollarEnemy = {
+  ...Enemy,
   name: 'Big Money',
   health: 5,
   damage: 1,
-  className: 'enemy',
   character: '$'
 };
