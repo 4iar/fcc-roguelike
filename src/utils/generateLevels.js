769a4fe945110bfd70b3cbacb9f36c9dd9280d9e
4iar <4iar@users.noreply.github.com>
2016-07-23 21:37:53 +0100

    Add generateLevels scaffolding

--- src/utils/generateLevels.js
+++ src/utils/generateLevels.js
@@ -1,8 +1,11 @@
 import _ from 'lodash';
 
+import makeRect from './makeRect';
 import populateLadders from './populateLadders';
+import { floorEntity } from '../constants/entityTypes';
 
 import { Floor } from '../entities/floor';
+import { Wall } from '../entities/obstacles';
 import { Player } from '../entities/player';
 import { DollarEnemy }  from '../entities/enemies';
 import { LadderUp, LadderDown } from '../entities/ladders';
@@ -10,44 +13,54 @@ import { LadderUp, LadderDown } from '../entities/ladders';
 const BOARD_DIMENSIONS = [50, 50];
 const NUMBER_OF_LEVELS = 3;
 
-function generateLevel(ladders={up: false, down: false}) {
-  let spawnCoordinates = [];
-
+function generateEmptyLevel() {
   let board = [];
   for (let row = 0; row < BOARD_DIMENSIONS[0]; row++) {
     board.push([]);
     for (let col = 0; col < BOARD_DIMENSIONS[1]; col++) {
-      board[row][col] = Floor;
+      board[row][col] = {character: ' '};  // TODO: use an empty space/void entity
     }
   }
 
-  // TODO: factor these out
-  if (ladders.up) {
-    const ladderUpCoords = [_.random(2, 45), _.random(2, 45)];
-    board[ladderUpCoords[0]][ladderUpCoords[1]] = LadderUp;
-    spawnCoordinates['up'] = [ladderUpCoords[0] + 1, ladderUpCoords[1]];
-  }
+  // TODO: use dynamic room sizes that account for different board dimensions
+  _.times(240, () => {
+    const middleRect = makeRect([_.random(1, 40), _.random(1, 40)], _.random(6, 9), _.random(6, 9));
+    middleRect.boundary.forEach((coords) => {
+      if (board[coords[0]][coords[1]].type !== floorEntity) {
+        board[coords[0]][coords[1]] = Wall;
+      }
+    })
 
-  if (ladders.down) {
-    const ladderDownCoords = [_.random(2, 45), _.random(2, 45)];
-    board[ladderDownCoords[0]][ladderDownCoords[1]] = LadderDown;
-    spawnCoordinates['down'] = [ladderDownCoords[0] - 1, ladderDownCoords[1]];
-  }
-  populateLadders({board}, {up: true, down: true});
-  
-  return {
-    board,
-    spawnCoordinates
-  };
+    middleRect.inner.forEach((coords) => {
+      board[coords[0]][coords[1]] = Floor;
+    })
+  })
+
+  return board;
 }
 
+function generateLevel() {
+  const board = generateEmptyLevel();
+  const centre = [Math.floor(50/2), Math.floor(50/2)];
+  
+  let level = populateLadders({board}, {up: true, down: true});
+  
+  return {
+    ...level,
+    board: level.board,
+    spawnCoordinates: {
+      ...level.spawnCoordinates,
+     player: centre
+    }
+  }
+}
 
 export default function generateLevels() {
   let levels = [];
   for (let levelNumber = 0; levelNumber < NUMBER_OF_LEVELS; levelNumber++) {
-    levels.push(generateLevel({down: true, up: true}));
+    levels.push(generateLevel());
   }
-  
+
   // placeholder for testing
   levels[0].board[0][0] = _.clone(Player);
   levels[0].spawnCoordinates.player = [0, 0]
